6c3ef2a7d77ca3c1563620cd4839d50835082514
吴尽思 <wfh2011@163.com>
2026-03-30 22:15:11 +0800

    fix: add s3_utils

--- src/s3/s3_utils.cpp
+++ src/s3/s3_utils.cpp
@@ -8,27 +8,12 @@
 #include <iomanip>
 #include <iostream>
 #include <sstream>
+#include <type_traits>
 
 static const char* RFC822_DATE_FORMAT_STR_MINUS_Z = "%a, %d %b %Y %H:%M:%S";
 static const char* RFC822_DATE_FORMAT_STR_WITH_Z = "%a, %d %b %Y %H:%M:%S %Z";
 static const char* ISO_8601_LONG_DATE_FORMAT_STR = "%Y-%m-%dT%H:%M:%SZ";
 static const char* ISO_8601_LONG_BASIC_DATE_FORMAT_STR = "%Y%m%dT%H%M%SZ";
-
-std::tm CreateZeroedTm()
-{
-    std::tm timeStruct;
-    timeStruct.tm_hour = 0;
-    timeStruct.tm_isdst = -1;
-    timeStruct.tm_mday = 0;
-    timeStruct.tm_min = 0;
-    timeStruct.tm_mon = 0;
-    timeStruct.tm_sec = 0;
-    timeStruct.tm_wday = 0;
-    timeStruct.tm_yday = 0;
-    timeStruct.tm_year = 0;
-
-    return timeStruct;
-}
 
 // Get the 0-6 week day number from a string representing WeekDay. Case insensitive and will stop on abbreviation
 static int GetWeekDayNumberFromStr(const char* timeString, size_t startIndex, size_t stopIndex)
@@ -385,6 +370,21 @@ time_t TimeGM(struct tm* const t)
 // Ensure local classes with generic names have internal linkage
 namespace
 {
+    std::tm CreateZeroedTm()
+    {
+        std::tm timeStruct;
+        timeStruct.tm_hour = 0;
+        timeStruct.tm_isdst = -1;
+        timeStruct.tm_mday = 0;
+        timeStruct.tm_min = 0;
+        timeStruct.tm_mon = 0;
+        timeStruct.tm_sec = 0;
+        timeStruct.tm_wday = 0;
+        timeStruct.tm_yday = 0;
+        timeStruct.tm_year = 0;
+
+        return timeStruct;
+    }
 
     class DateParser
     {
@@ -1571,5 +1571,355 @@ namespace s3
             return gmtTimeStamp;
         }
 
+        void StringUtils::Replace(std::string& s, const char* search, const char* replace)
+        {
+            if (!search || !replace)
+            {
+                return;
+            }
+
+            size_t replaceLength = strlen(replace);
+            size_t searchLength = strlen(search);
+
+            for (std::size_t pos = 0;; pos += replaceLength)
+            {
+                pos = s.find(search, pos);
+                if (pos == std::string::npos)
+                    break;
+
+                s.erase(pos, searchLength);
+                s.insert(pos, replace);
+            }
+        }
+
+        std::string StringUtils::ToLower(const char* source)
+        {
+            std::string copy;
+            size_t sourceLength = strlen(source);
+            copy.resize(sourceLength);
+            // appease the latest whims of the VC++ 2017 gods
+            std::transform(source, source + sourceLength, copy.begin(), [](unsigned char c) { return (char)::tolower(c); });
+
+            return copy;
+        }
+
+        std::string StringUtils::ToUpper(const char* source)
+        {
+            std::string copy;
+            size_t sourceLength = strlen(source);
+            copy.resize(sourceLength);
+            // appease the latest whims of the VC++ 2017 gods
+            std::transform(source, source + sourceLength, copy.begin(), [](unsigned char c) { return (char)::toupper(c); });
+
+            return copy;
+        }
+
+        bool StringUtils::CaselessCompare(const char* value1, const char* value2)
+        {
+            std::string value1Lower = ToLower(value1);
+            std::string value2Lower = ToLower(value2);
+
+            return value1Lower == value2Lower;
+        }
+
+        std::vector<std::string> StringUtils::Split(const std::string& toSplit, char splitOn)
+        {
+            return Split(toSplit, splitOn, SIZE_MAX, SplitOptions::NOT_SET);
+        }
+
+        std::vector<std::string> StringUtils::Split(const std::string& toSplit, char splitOn, SplitOptions option)
+        {
+            return Split(toSplit, splitOn, SIZE_MAX, option);
+        }
+
+        std::vector<std::string> StringUtils::Split(const std::string& toSplit, char splitOn, size_t numOfTargetParts)
+        {
+            return Split(toSplit, splitOn, numOfTargetParts, SplitOptions::NOT_SET);
+        }
+
+        std::vector<std::string> StringUtils::Split(const std::string& toSplit, char splitOn, size_t numOfTargetParts, SplitOptions option)
+        {
+            if (option == SplitOptions::INCLUDE_EMPTY_SEGMENTS)
+            {
+                return StringUtils::SplitWithSpaces(toSplit, splitOn);
+            }
+
+            std::vector<std::string> returnValues;
+            std::stringstream input(toSplit);
+            std::string item;
+
+            while (returnValues.size() < numOfTargetParts - 1 && std::getline(input, item, splitOn))
+            {
+                if (!item.empty() || option == SplitOptions::INCLUDE_EMPTY_ENTRIES)
+                {
+                    returnValues.emplace_back(std::move(item));
+                }
+            }
+
+            if (std::getline(input, item, static_cast<char>(EOF)))
+            {
+                if (option != SplitOptions::INCLUDE_EMPTY_ENTRIES)
+                {
+                    // Trim all leading delimiters.
+                    item.erase(item.begin(), std::find_if(item.begin(), item.end(), [splitOn](int ch) { return ch != splitOn; }));
+                    if (!item.empty())
+                    {
+                        returnValues.emplace_back(std::move(item));
+                    }
+                }
+                else
+                {
+                    returnValues.emplace_back(std::move(item));
+                }
+            }
+            // To handle the case when there are trailing delimiters.
+            else if (!toSplit.empty() && toSplit.back() == splitOn && option == SplitOptions::INCLUDE_EMPTY_ENTRIES)
+            {
+                returnValues.emplace_back();
+            }
+
+            return returnValues;
+        }
+
+        std::vector<std::string> StringUtils::SplitWithSpaces(const std::string& toSplit, char splitOn)
+        {
+            size_t pos = 0;
+            std::string split{toSplit};
+            std::vector<std::string> returnValues;
+            while ((pos = split.find(splitOn)) != std::string::npos)
+            {
+                returnValues.emplace_back(split.substr(0, pos));
+                split.erase(0, pos + 1);
+            }
+            if (!split.empty())
+            {
+                returnValues.emplace_back(split);
+            }
+            return returnValues;
+        }
+
+        std::vector<std::string> StringUtils::SplitOnLine(const std::string& toSplit)
+        {
+            std::stringstream input(toSplit);
+            std::vector<std::string> returnValues;
+            std::string item;
+
+            while (std::getline(input, item))
+            {
+                if (item.size() > 0)
+                {
+                    returnValues.push_back(item);
+                }
+            }
+
+            return returnValues;
+        }
+
+        std::string StringUtils::URLEncode(const char* unsafe)
+        {
+            std::stringstream escaped;
+            escaped.fill('0');
+            escaped << std::hex << std::uppercase;
+
+            size_t unsafeLength = strlen(unsafe);
+            for (auto i = unsafe, n = unsafe + unsafeLength; i != n; ++i)
+            {
+                char c = *i;
+                if (IsAlnum(c) || c == '-' || c == '_' || c == '.' || c == '~')
+                {
+                    escaped << (char)c;
+                }
+                else
+                {
+                    // this unsigned char cast allows us to handle unicode characters.
+                    escaped << '%' << std::setw(2) << int((unsigned char)c) << std::setw(0);
+                }
+            }
+
+            return escaped.str();
+        }
+
+        std::string StringUtils::UTF8Escape(const char* unicodeString, const char* delimiter)
+        {
+            std::stringstream escaped;
+            escaped.fill('0');
+            escaped << std::hex << std::uppercase;
+
+            size_t unsafeLength = strlen(unicodeString);
+            for (auto i = unicodeString, n = unicodeString + unsafeLength; i != n; ++i)
+            {
+                int c = *i;
+                if (c >= ' ' && c < 127)
+                {
+                    escaped << (char)c;
+                }
+                else
+                {
+                    // this unsigned char cast allows us to handle unicode characters.
+                    escaped << delimiter << std::setw(2) << int((unsigned char)c) << std::setw(0);
+                }
+            }
+
+            return escaped.str();
+        }
+
+        std::string StringUtils::URLEncode(double unsafe)
+        {
+            char buffer[32];
+            snprintf(buffer, sizeof(buffer), "%g", unsafe);
+
+            return StringUtils::URLEncode(buffer);
+        }
+
+        std::string StringUtils::URLDecode(const char* safe)
+        {
+            std::string unescaped;
+
+            for (; *safe; safe++)
+            {
+                switch (*safe)
+                {
+                case '%': {
+                    int hex = 0;
+                    auto ch = *++safe;
+                    if (ch >= '0' && ch <= '9')
+                    {
+                        hex = (ch - '0') * 16;
+                    }
+                    else if (ch >= 'A' && ch <= 'F')
+                    {
+                        hex = (ch - 'A' + 10) * 16;
+                    }
+                    else if (ch >= 'a' && ch <= 'f')
+                    {
+                        hex = (ch - 'a' + 10) * 16;
+                    }
+                    else
+                    {
+                        unescaped.push_back('%');
+                        if (ch == 0)
+                        {
+                            return unescaped;
+                        }
+                        unescaped.push_back(ch);
+                        break;
+                    }
+
+                    ch = *++safe;
+                    if (ch >= '0' && ch <= '9')
+                    {
+                        hex += (ch - '0');
+                    }
+                    else if (ch >= 'A' && ch <= 'F')
+                    {
+                        hex += (ch - 'A' + 10);
+                    }
+                    else if (ch >= 'a' && ch <= 'f')
+                    {
+                        hex += (ch - 'a' + 10);
+                    }
+                    else
+                    {
+                        unescaped.push_back('%');
+                        unescaped.push_back(*(safe - 1));
+                        if (ch == 0)
+                        {
+                            return unescaped;
+                        }
+                        unescaped.push_back(ch);
+                        break;
+                    }
+
+                    unescaped.push_back(char(hex));
+                    break;
+                }
+                case '+':
+                    unescaped.push_back(' ');
+                    break;
+                default:
+                    unescaped.push_back(*safe);
+                    break;
+                }
+            }
+
+            return unescaped;
+        }
+
+        static bool IsSpace(int ch)
+        {
+            if (ch < -1 || ch > 255)
+            {
+                return false;
+            }
+
+            return ::isspace(ch) != 0;
+        }
+
+        std::string StringUtils::LTrim(const char* source)
+        {
+            std::string copy(source);
+            copy.erase(copy.begin(), std::find_if(copy.begin(), copy.end(), [](int ch) { return !IsSpace(ch); }));
+            return copy;
+        }
+
+        // trim from end
+        std::string StringUtils::RTrim(const char* source)
+        {
+            std::string copy(source);
+            copy.erase(std::find_if(copy.rbegin(), copy.rend(), [](int ch) { return !IsSpace(ch); }).base(), copy.end());
+            return copy;
+        }
+
+        // trim from both ends
+        std::string StringUtils::Trim(const char* source)
+        {
+            return LTrim(RTrim(source).c_str());
+        }
+
+        long long StringUtils::ConvertToInt64(const char* source)
+        {
+            if (!source)
+            {
+                return 0;
+            }
+
+            return std::atoll(source);
+        }
+
+        long StringUtils::ConvertToInt32(const char* source)
+        {
+            if (!source)
+            {
+                return 0;
+            }
+
+            return std::atol(source);
+        }
+
+        bool StringUtils::ConvertToBool(const char* source)
+        {
+            if (!source)
+            {
+                return false;
+            }
+
+            std::string strValue = ToLower(source);
+            if (strValue == "true" || strValue == "1")
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        double StringUtils::ConvertToDouble(const char* source)
+        {
+            if (!source)
+            {
+                return 0.0;
+            }
+
+            return std::strtod(source, NULL);
+        }
     } // namespace utils
 } // namespace s3
--- src/s3/s3_utils.hpp
+++ src/s3/s3_utils.hpp
@@ -271,6 +271,193 @@ namespace s3
             tm ConvertTimestampToGmtStruct() const;
         };
 
+        /**
+         * All the things the c++ stdlib is missing for string operations that I needed.
+         */
+        class StringUtils
+        {
+        public:
+            static void Replace(std::string& s, const char* search, const char* replace);
+
+            /**
+             * Converts a string to lower case.
+             */
+            static std::string ToLower(const char* source);
+
+            /**
+             * Converts a string to upper case.
+             */
+            static std::string ToUpper(const char* source);
+
+            /**
+             * Does a caseless comparison of two strings.
+             */
+            static bool CaselessCompare(const char* value1, const char* value2);
+
+            /**
+             * URL encodes a string (uses %20 not + for spaces).
+             */
+            static std::string URLEncode(const char* unsafe);
+
+            static inline std::string URLEncode(const std::string& unsafe)
+            {
+                return URLEncode(unsafe.c_str());
+            }
+
+            /**
+             * Http Clients tend to escape some characters but not all. Escaping all of them causes problems, because the client
+             * will also try to escape them.
+             * So this only escapes non-ascii characters and the + character
+             */
+            static std::string UTF8Escape(const char* unicodeString, const char* delimiter);
+
+            /**
+             * URL encodes a double (if it ends up going to scientific notation) otherwise it just returns it as a string.
+             */
+            static std::string URLEncode(double unsafe);
+
+            /**
+             * Decodes a URL encoded string (will handle both encoding schemes for spaces).
+             */
+            static std::string URLDecode(const char* safe);
+
+            enum class SplitOptions
+            {
+                /**
+                 * By default, removes all the empty entries in the vector returned by Split()
+                 */
+                NOT_SET,
+                /**
+                 * Deprecated use INCLUDE_EMPTY_SEGMENTS instead.
+                 */
+                INCLUDE_EMPTY_ENTRIES,
+                /**
+                 * Include delimiters as empty segments in the split string
+                 */
+                INCLUDE_EMPTY_SEGMENTS,
+            };
+
+            /**
+             * @brief Splits a string on a delimiter (empty items are excluded).
+             * @param toSplit, the original string to split
+             * @param splitOn, the delimiter you want to use.
+             */
+            static std::vector<std::string> Split(const std::string& toSplit, char splitOn);
+
+            /**
+             * @brief Splits a string on a delimiter.
+             * @param toSplit, the original string to split
+             * @param splitOn, the delimiter you want to use.
+             * @param option, if INCLUDE_EMPTY_ENTRIES, includes empty entries in the result, otherwise removes empty entries.
+             */
+            static std::vector<std::string> Split(const std::string& toSplit, char splitOn, SplitOptions option);
+
+            /**
+             * @brief Splits a string on a delimiter (empty items are excluded).
+             * @param toSplit, the original string to split
+             * @param splitOn, the delimiter you want to use.
+             * @param numOfTargetParts, how many target parts you want to get, if it is 0, as many as possible.
+             */
+            static std::vector<std::string> Split(const std::string& toSplit, char splitOn, size_t numOfTargetParts);
+
+            /**
+             * @brief Splits a string on a delimiter.
+             * @param toSplit, the original string to split
+             * @param splitOn, the delimiter you want to use.
+             * @param numOfTargetParts, how many target parts you want to get, if it is 0, as many as possible.
+             * @param option, if INCLUDE_EMPTY_ENTRIES, includes empty entries in the result, otherwise removes empty entries.
+             */
+            static std::vector<std::string> Split(const std::string& toSplit, char splitOn, size_t numOfTargetParts, SplitOptions option);
+
+            /**
+             * Splits a string on delimeter, keeping the delimiter in the string as a empty space.
+             * @param toSplit, the original string to split
+             * @param splitOn, the delimiter you want to use.
+             */
+            static std::vector<std::string> SplitWithSpaces(const std::string& toSplit, char splitOn);
+
+            /**
+             * Splits a string on new line characters.
+             */
+            static std::vector<std::string> SplitOnLine(const std::string& toSplit);
+
+            /** static std::vector<std::string> SplitOnRegex(std::string regex);
+             *  trim from start
+             */
+            static std::string LTrim(const char* source);
+
+            /**
+             * trim from end
+             */
+            static std::string RTrim(const char* source);
+
+            /**
+             * trim from both ends
+             */
+            static std::string Trim(const char* source);
+
+            /**
+             * convert to int 64
+             */
+            static long long ConvertToInt64(const char* source);
+
+            /**
+             * convert to int 32
+             */
+            static long ConvertToInt32(const char* source);
+
+            /**
+             * convert to bool
+             */
+            static bool ConvertToBool(const char* source);
+
+            /**
+             * convert to double
+             */
+            static double ConvertToDouble(const char* source);
+
+            /**
+             * not all platforms (Android) have std::to_string
+             */
+            template <typename T> static std::string to_string(T value)
+            {
+                std::ostringstream os;
+                os << value;
+                return os.str();
+            }
+
+            /**
+             * locale agnostic implementation of std::isalnum
+             */
+            static bool IsAlnum(char c)
+            {
+                return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
+            }
+
+            /**
+             * Convert an unsigned integer to its hex string in upper case.
+             */
+            template <typename T, class = typename std::enable_if<std::is_unsigned<T>::value>::type> static std::string ToHexString(T value)
+            {
+                if (value == 0)
+                {
+                    return "0";
+                }
+
+                std::string s;
+                s.reserve(sizeof(value) * 2);
+                T r = value;
+                while (r > 0)
+                {
+                    s += "0123456789ABCDEF"[r & 0xf];
+                    r >>= 4;
+                }
+
+                std::reverse(s.begin(), s.end());
+                return s;
+            }
+        };
+
     } // namespace utils
 
 } // namespace s3
